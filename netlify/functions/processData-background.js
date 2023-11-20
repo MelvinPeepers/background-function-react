1cedfa071003b8a54beaa686d30d6f240fa89a3f
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-11-20 04:49:57 -0600

    added background function

---
+++ netlify/functions/processData-background.js
@@ -0,0 +1,28 @@
+// processData.js
+
+exports.handler = async function (event) {
+  try {
+    // Simulate processing time
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    // Here, you can perform any asynchronous tasks or computations
+    // For demonstration, let's log the incoming event data
+    console.log("Incoming event data:", event);
+
+    // Return a response or success message
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Background function executed successfully",
+      }),
+    };
+  } catch (error) {
+    // Handle errors gracefully
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "An error occurred during function execution",
+      }),
+    };
+  }
+};
